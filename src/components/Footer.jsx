10f4d1c2e29a4dfb8068f24ce461b30648db274c
Claudio D. Morales <mclaudiod@gmail.com>
2024-09-13 18:41:29 -0300

    Footer margin fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="relative bottom-0 left-0 right-0 footer border-t border-zinc-700 z-50 bg-zinc-950 bg-opacity-80">
-      <div className="container flex items-center justify-between">
+      <div className="container mx-auto flex items-center justify-between">
         <img className="w-[200px] my-12 mx-3" src="/Clau Logo final A1.png" alt="Logo" />
         <p className="text-zinc-600">
           © 2024{" "}
